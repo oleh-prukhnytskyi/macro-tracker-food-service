01fb9f98e01f250b75c9b5b6f33e3bbeb29a3d09
Oleg Pruh <olegpruh1@gmail.com>
2025-12-05 00:28:44 +0000

    Update common repository version

--- src/main/java/com/olehprukhnytskyi/macrotrackerfoodservice/MacroTrackerFoodServiceApplication.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackerfoodservice/MacroTrackerFoodServiceApplication.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -16,6 +19,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                         + "search and nutrition information"
         )
 )
+@EnableJpaRepositories(basePackages = {
+        "com.olehprukhnytskyi.repository.jpa"
+})
+@EnableMongoRepositories(basePackages = {
+        "com.olehprukhnytskyi.macrotrackerfoodservice.repository.mongo"
+})
+@EntityScan(basePackages = {
+        "com.olehprukhnytskyi.macrotrackerfoodservice.model",
+        "com.olehprukhnytskyi.model"
+})
 @EnableCaching
 @EnableFeignClients
 @SpringBootApplication
